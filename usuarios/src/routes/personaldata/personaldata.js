a9b04d58155f99c73da285c83d515b483473b43c
João Vitor Andrade <joaoandradeprime99@gmail.com>
2024-06-20 15:01:38 -0300

    Feature: Finalização do conteúdo da rota personalData

--- usuarios/src/routes/personaldata/personaldata.js
+++ usuarios/src/routes/personaldata/personaldata.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const routerPersonal = express.Router()
 const data = require('../../database/config.js')
-const router = require('../users/users')
+const verificar = require("../../middleware/verify_token.js")
 
-routerPersonal.get("/listar", (req, res) => {
+routerPersonal.get("/listar", verificar, (req, res) => {
     data.query("select * from dadospessoais", (error, result) => {
         if (error) {
             return res.status(500).send({ msg: "Erro ao selecionar os dados" })
@@ -13,7 +13,7 @@ routerPersonal.get("/listar", (req, res) => {
     })
 })
 
-routerPersonal.get("/listar/:cpf", (req, res) => {
+routerPersonal.get("/listar/:cpf", verificar, (req, res) => {
     data.query("select * from dadospessoais where iddadospessoais=?", req.params.cpf, (error, result) => {
         if (error) {
             return res.status(500).send({ msg: "Erro ao selecionar os dados" })
@@ -22,7 +22,7 @@ routerPersonal.get("/listar/:cpf", (req, res) => {
     })
 })
 
-routerPersonal.post("/cadastrar", (req, res) => {
+routerPersonal.post("/cadastrar", verificar, (req, res) => {
     data.query("insert into dadospessoais set ?", req.body, (error, result) => {
         if (error) {
             return res.status(500).send({ msg: "Erro ao cadastrar os dados" })
@@ -30,3 +30,14 @@ routerPersonal.post("/cadastrar", (req, res) => {
         res.status(201).send({ msg: "Criado", payload: result })
     })
 })
+
+routerPersonal.put("/atualizar/:id", verificar, (req, res) => {
+    data.query("update dadospessoais set ? where iddadospessoais=?", [req.body, req.params.id], (error, result) => {
+        if (error) {
+            return res.status(500).send({ msg: "Erro ao atualizar os dados" })
+        }
+        res.status(200).send({ msg: "Atualizado", payload: result })
+    })
+})
+
+module.exports = routerPersonal
