b3b9e5f7d29e4b30a697b3cf194997980a6df29f
João Vitor Andrade <joaoandradeprime99@gmail.com>
2024-06-21 15:28:34 -0300

    Feature: Criação dos objetos fotos e precos

--- livros/src/index.js
+++ livros/src/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const routerTitulo = require("./routes/titulos/titulos.js")
+const routerPreco = require("./routes/precos/precos.js")
+const routerFoto = require("./routes/fotos/fotos.js")
 
 const app = express()
 app.use(express.json())
 
 app.use("/api/v1/livros", routerTitulo)
+app.use("/api/v1/precos", routerPreco)
+app.use("/api/v1/fotos", routerFoto)
 
 app.listen(process.env.HOST_PORT, () => console.log(`Servidor Online em ${process.env.HOST_NAME}:${process.env.HOST_PORT}`))
