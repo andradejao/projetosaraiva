b0b62e5588aeb13bacae8cd4ce2b7ba30b7fdfde
João Vitor Andrade <joaoandradeprime99@gmail.com>
2024-07-24 17:03:15 -0300

    update

--- carrinho/src/routes/carrinho/carrinho.js
+++ carrinho/src/routes/carrinho/carrinho.js
@@ -11,4 +11,27 @@ routerCarrinho.get("/listar", (req, res) => {
     })
 })
 
+routerCarrinho.get("/listar/:id", (req, res) => {
+    data.query(`select foto.foto1,titulo.nometitulo,titulo.autor,carrinho.quantidade,
+    preco.precodesconto,titulo.idtitulo,carrinho.total
+    from saraivalivrodb.fotos foto inner join saraivalivrodb.titulos titulo
+    on foto.idfotos=titulo.idfoto inner join saraivacarrinhodb.carrinho carrinho
+    on titulo.idtitulo=carrinho.idproduto inner join saraivalivrodb.precos preco
+    on preco.idpreco = titulo.idpreco where carrinho.idusuario=?;`, req.params.id, (error, dados) => {
+        if (error) {
+            return res.status(500).send({ msg: "Erro ao carregar os dados" })
+        }
+        res.status(200).send({ msg: "Ok", payload: dados })
+    })
+})
+
+routerCarrinho.post("/cadastrar", (req, res) => {
+    data.query("insert into carrinho set ?", req.body, (error, result) => {
+        if (error) {
+            return res.status(500).send({ msg: "Não foi possível adicionar ao carrinho" })
+        }
+        res.status(201).send({ msg: "Criado", payload: result })
+    })
+})
+
 module.exports = routerCarrinho
